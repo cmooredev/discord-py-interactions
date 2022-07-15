e16ba7779f3857e67ba0ed8bb031ca6c48f8558d
cmooredev <46297716+cmooredev@users.noreply.github.com>
2022-07-15 07:50:19 -0700

    update for second video

--- main.py
+++ main.py
@@ -1,23 +1,45 @@
 import discord
 import asyncio
 import config
+#--------- NEW LINE
+from discord.ext import commands
+import requests
+#---------
 
 intents = discord.Intents.default()
 intents.message_content = True
-client = discord.Client(intents=intents)
+#----change this line
+#client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='.', intents=intents)
 
 
-#event 
+#event
 #when bot is loaded and ready this will call
-@client.event
+
+#change this from client to bot
+#@client.event
+@bot.event
 async def on_ready():
     print('Online.')
 
-@client.event
+
+#@client.event
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
+    #if message.author == client.user:
+    await bot.process_commands(message)
+    if message[0] == '.':
+        return
+    if message.author == bot.user:
         return
     await message.channel.send("whats up!")
+
+
+#-------ADD
+@bot.command()
+async def facts(ctx, number):
+    response = requests.get(f'http://numbersapi.com/{number}')
+    await ctx.channel.send(response.text)
 
 #finishes running this before starting the bot
 #fill this with any other work you need to do in the background
@@ -28,6 +50,7 @@ async def setup():
 
 async def main():
     await setup()
-    await client.start(config.TOKEN)
+    #await client.start(config.TOKEN)
+    await bot.start(config.TOKEN)
 
 asyncio.run(main())
